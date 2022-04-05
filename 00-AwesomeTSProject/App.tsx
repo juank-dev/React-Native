30b9911819d7c8a5aead81e0496bfbf8785aaeb7
Juan Carlos Lopez <55102476+juank-dev@users.noreply.github.com>
2022-04-04 21:09:35 -0500

    archivos inicial para React Native

---
+++ 00-AwesomeTSProject/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo desde mi Movil 123</Text>
+    </View>
+  );
+};
+
+export default App;
